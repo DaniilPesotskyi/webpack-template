e5886cfe098ac199ee82749b27b7e26aaae31cec
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-04-29 22:56:23 +0300

    ghp try

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.js',
-    publicPath: "/webpack-template/"
+    // publicPath: "/webpack-template/"
   },
   module: {
     rules: [
